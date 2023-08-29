cacafe222f6461f324e506a1a43e027b1922b2f6
TissaMaria <142171515+TissaMaria@users.noreply.github.com>
2023-08-29 22:08:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 
-  // Carousel
+  
 let slideIndex = 0;
 showSlides();
 
@@ -13,5 +13,5 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 4000); // Change image every 2 seconds
+  setTimeout(showSlides, 4000); 
 }
